f8b5fb00431709b9bc7448f6b9b7f8c15dffb422
CJPayn <164914136+CJPayn@users.noreply.github.com>
2024-04-13 13:37:54 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ function testFunction() {
 }
 
 const photoFrame = document.getElementById("photo-frame")
+const oneCrestType = document.getElementById("crest-one")
+const twoCrestType = document.getElementById("crest-two")
+const oneCrestSelection = document.getElementById("crest-one-selection")
+const twoCrestSelection = document.getElementById("crest-two-selection")
+const crestOnePlace = document.getElementById("crest-one-place")
+const crestTwoPlace = document.getElementById("crest-two-place")
+let crestTy = ""
+let crestSel = ""
+let crest = ""
 let focus = false
 let selImage = document.getElementById("selected-image")
 let tgtImage = document.getElementById("group-photo")
@@ -14,11 +23,14 @@ let zoomSlider = document.getElementById("zoom")
 let zoomSliderState = zoomSlider.value
 let imagePosition = [0,0]
 tgtImage.style.transform = `scale(1) rotate(0deg)`
+oneCrestSelection.style.display = "none"
+twoCrestSelection.style.display = "none"
 
 function imgUpload() {
 	let files = document.getElementById("selected-image").files[0]
 	let src = URL.createObjectURL(files);
 	photoFrame.innerHTML = `<img id="group-photo" width="800" src="${src}">`
+	resetImage()
       
 }
 
@@ -70,6 +82,64 @@ rotationSlider.addEventListener("input", (event) => {
 	rotationSliderState = rotationSlider.value
   tgtImage.style.transform = `scale(1.${zoomSliderState})  rotate(${rotationSliderState}deg)`
 });
+
+function crestType(input) {
+	if (input === "one") {
+		crestTy = oneCrestType
+		crestSel = oneCrestSelection
+	} else if (input === "two"){
+		crestTy = twoCrestType
+		crestSel = twoCrestSelection
+	}
+	
+	if (crestTy.value === "") {
+		crestSel.style.display = "none"
+	} else if (crestTy.value === "stn") {
+		crestSel.style.display = "inline"
+		crestSel.innerHTML = `
+					<option value="">Select Crest</option>
+					<option value="aki">Akrotiri</option>
+					<option value="ald">Aldergrove</option>
+					<option value="ath">St Athan</option>
+					<option value="ben">Benson</option>
+					<option value="bzn">Brize Norton</option>
+					<option value="coni">Coningsby</option>
+					<option value="cos">Cosford</option>
+					<option value="crn">Cranwell</option>
+					<option value="gib">Gibraltar</option>
+					<option value="hal">Halton</option>
+					<option value="hon">Honington</option>
+					<option value="lee">Leeming</option>
+					<option value="los">Lossiemouth</option>
+					<option value="mar">Marham</option>
+					<option value="maw">St Mawgan</option>
+					<option value="mpa">Mount Pleasant</option>
+					<option value="nor">Northolt</option>
+					<option value="odi">Odiham</option>
+					<option value="sca">Scampton</option>
+					<option value="sha">Shawbury</option>
+					<option value="val">Valley</option>
+					<option value="wad">Waddington</option>
+					<option value="wit">Wittering</option>
+					<option value="wyt">Wyton</option>`
+	} else if (crestTy.value === "sqn") {
+		crestSel.style.display = "inline"
+		crestSel.innerHTML = `
+					<option value="">Select Crest</option>
+					<option value="1sqn">1 Sqn</option>
+					<option value="2sqn">2 Sqn</option>`
+	}
+}
+
+function crestSelection(input) {
+	if (input === "one") {
+		crest = oneCrestSelection.value
+		crestOnePlace.src = `img/${crest}.png`
+	} else if (input === "two") {
+		crest = twoCrestSelection.value
+		crestTwoPlace.src = `img/${crest}.png`
+	}
+}
 
 //rotationSlider.addEventListener("input", (event) => {
 //	rotationSliderState = rotationSlider.value
